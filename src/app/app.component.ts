8ddfd234dc9d17f3d96b0a38e72c24060680bcfc
= <=>
2023-03-22 19:16:57 +0530

    Save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-himanshu',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Todo-App';
+export class MyComponent {
+  listUserTask: any[] = [];
+  addTask(value: string) {
+    this.listUserTask.push({ id: this.listUserTask.length, task: value })
+    console.log(this.listUserTask);
+  }
+  removeTask(id: Number) {
+    this.listUserTask = this.listUserTask.filter(i => i.id !== id);
+  }
+  doneTask(id: Number) {
+    taskUser : String;
+    console.log()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MyComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MyComponent]
 })
 export class AppModule { }
